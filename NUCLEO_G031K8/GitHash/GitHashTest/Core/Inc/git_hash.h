91e54d835265b561fab8f58b026ef181990d089d
unknown <akashchandrasr@gmail.com>
2025-09-30 19:43:21 -0700

    removed bad file

--- NUCLEO_G031K8/GitHash/GitHashTest/Core/Inc/git_hash.h
+++
@@ -1,7 +0,0 @@
-#ifndef GIT_HASH_H
-#define GIT_HASH_H
-// http://xit0.org/2013/04/cmake-use-git-branch-and-commit-details-in-project/
-#define GIT_BRANCH "main"
-#define GIT_COMMIT_HASH "5fe9d60da73007c1aea982a9b845e77acd659098"
-
-#endif
